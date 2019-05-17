8b8c544fce1837151f680d77ca269bd75aa0218f
Bogacenco Artiom <artyombogachenco@gmail.com>
2019-05-17 17:40:08 +0300

    menu update

--- menu.py
+++ menu.py
@@ -1,9 +1,10 @@
 import cocos
 from cocos.director import director
-
+from cocos.scenes import FlipX3DTransition # Анимация перехода
 class MainMenu(cocos.menu.Menu):
     
-    def __init__(self):
+    def __init__(self, main_game_scene):
+        self.main_game_scene = main_game_scene
         super().__init__("Peace Data quest") # Название 
 
         menus = []
@@ -22,8 +23,8 @@ class MainMenu(cocos.menu.Menu):
     """Классы для взаимодействия с пунктами меню"""
 
     def on_new_game(self): 
-        """Здесь будет выполняться загрузака новой игры"""
-        print("Игра началась")
+        """Здесь будет выполняться загрузка новой игры"""
+        director.replace(FlipX3DTransition(self.main_game_scene, duration = 2))
 
     def on_quit(self):
         """Выход"""
@@ -32,14 +33,3 @@ class MainMenu(cocos.menu.Menu):
     def on_show_fps(self, show_fps):
         """Показать FPS"""
         director.show_FPS = show_fps
-
-
-if __name__ == '__main__':
-    director.init(width=1280, height=720, caption="Peace Data menu test") # Инициализация директора (тест)
-    director.window.pop_handlers() # Отключение дебаг функций
-    
-    menu = MainMenu() # Создание объекта "Меню"
-    test_scene = cocos.scene.Scene() # Тестовая сцена
-    test_scene.add(menu, z=0) # Добавление Меню на 0 слой
-
-    director.run(test_scene) # Запуск сцены
